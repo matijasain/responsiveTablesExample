eaaa156379a2c3ca7760605f2e2561d408dbec58
Matija Sain <matijasain@Matijas-MacBook-Air.local>
2021-01-24 09:40:46 +0100

    update

--- main.js
+++ main.js
@@ -1,32 +1,17 @@
-// Get the elements with class="column"
-var elements = document.getElementsByClassName('column');
-
-// // Declare a loop variable
-// var i;
-
-// // List View
-// function listView() {
-//   for (i = 0; i < elements.length; i++) {
-//     elements[i].style.width = '100%';
-//   }
-// }
-
-// // Grid View
-// function gridView() {
-//   for (i = 0; i < elements.length; i++) {
-//     elements[i].style.width = '50%';
-//   }
-// }
-
-function myFunction() {
+function mainFunction() {
+  const headers = document.getElementById('headers');
   const elements = document.getElementsByClassName('column');
   const button = document.getElementById('button');
 
-  console.log('adsas');
+  // column headers toggle
+  headers.classList.toggle('grid-headers');
+
+  // grid class toggle
   for (i = 0; i < elements.length; i++) {
     elements[i].classList.toggle('grid');
   }
 
+  // list/grid icon toggle
   if (button.innerHTML === 'view_module') {
     button.innerHTML = 'view_list';
   } else {
